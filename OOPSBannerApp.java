b2c4c05b069a79c5259c52393009e309aa51758d
Poobesh S <poobesh@Poobeshs-MacBook-Air.local>
2026-02-17 09:12:55 +0530

    UC-18: Completed ASCII banner display

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,18 @@
 /**
- * OOPS Banner App - UC1
+ * OOPS Banner App - UC2
+ * Purpose: Displaying "OOPS" as a large ASCII banner using a 7-line grid.
  * @author poobesh
- * @version 1.0
+ * @version 2.0
  */
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        // Main Method is the entry point
-        System.out.println("OOPS");
+        // UC2: Sequential print statements to form ASCII characters
+        System.out.println(" **** **** **** **** ");
+        System.out.println("* * * * * * * ");
+        System.out.println("* * * * * * *** ");
+        System.out.println("* * * * ***** * ");
+        System.out.println("* * * * * * ");
+        System.out.println(" **** **** * **** ");
+        System.out.println("              * ");
     }
 }
