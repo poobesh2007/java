d81a6c86a51eaef8f788b3066eab90ddab6c35ac
poobesh2007 <poobesh2007@gmail.com>
2026-02-17 09:29:45 +0530

    Create OOPSBannerApp uc 1.java

---
+++ OOPSBannerApp uc 1.java
@@ -0,0 +1,11 @@
+/**
+ * OOPS Banner App - UC1
+ * @author poobesh
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        // UC1 focuses on the main method as the entry point
+        System.out.println("OOPS");
+    }
+}
