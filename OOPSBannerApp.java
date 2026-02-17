0af676eb50f773db39bb43931e22c8531dd5c6bf
poobesh2007 <poobesh2007@gmail.com>
2026-02-17 08:40:16 +0530

    Update OOPSBannerApp.java

---
+++ OOPSBannerApp.java
@@ -0,0 +1,11 @@
+/**
+ * OOPS Banner App - UC1
+ * @author poobesh
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        // Main Method is the entry point
+        System.out.println("OOPS");
+    }
+}
